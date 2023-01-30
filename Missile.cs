2a29eb5578de90df35e13fef5ff4882e79af56c7
Zachary Adam Eason <zachary27.eason@gmail.com>
2023-01-30 16:44:32 -0600

    Fixed Missile Movement
    Created a more streamlined spawning process with random angles, still need to fix rotation.

--- Missile.cs
+++ Missile.cs
@@ -5,37 +5,28 @@ public class Missile : RigidBody2D
 {
 	private Vector2 velocity = new Vector2();
 	private Vector2 direction;
-	private Vector2 origin = new Vector2(0,0);
+	private Vector2 offset = new Vector2(0,0);
 
 	// Called when the node enters the scene tree for the first time.
 	
 	public override void _Ready()
 	{
-		uint randX = (GD.Randi() % 500) + 1;
-		uint randY = (GD.Randi() % 150) + 1;
-		int impX = (int) randX;
-		int impY = (int) randY;
-		if(GD.Randi() % 2 == 1)
+		for(int i = 1; i <= 6; i++)
 		{
-			impY = impY * -1;
+			Spawner sp = (Spawner)GetNode("/root/Stage/MissileControl/SpawnLoc" + i);
+			if(this.Position.x == sp.Position.x && this.Position.y == sp.Position.y)
+			{
+				ApplyImpulse(offset, sp.getSpawnerForce());
+			}
 		}
-		if(Position.x == 500)
-		{
-			impX = impX * -1;
-		}
-		Vector2 imp = new Vector2(impX, impY);
-		ApplyCentralImpulse(imp);
 	}
 	
 	public override void _Process(float delta)
 	{
-		direction = new Vector2(velocity.x, velocity.y).Normalized();
+		/*direction = new Vector2(velocity.x, velocity.y).Normalized();
 		float angle = direction.Angle();
-		this.GlobalRotation = Mathf.LerpAngle(this.GlobalRotation, angle, delta);
+		this.GlobalRotation = Mathf.LerpAngle(this.GlobalRotation, angle, delta);*/
 	}
 
-	public void applyImp(Vector2 imp)
-	{
-		this.ApplyCentralImpulse(imp);
-	}
+	
 }
--- MissileControl.cs
+++ MissileControl.cs
@@ -3,7 +3,7 @@ using System;
 
 public class MissileControl : Node2D
 {
-	private Node2D spawn;
+	private Spawner spawn;
 	public int randCounter = 0; //Controls how random the spawning will be
 	
 	public override void _Process(float delta)
@@ -14,21 +14,49 @@ public class MissileControl : Node2D
 		{
 			check = (GD.Randi() % 6) + 1;
 			int spawnLoc = (int) check;
+			setSpawner(spawnLoc);
 			spawnItem(spawnLoc);
 			randCounter = 0;
+		}
+	}
+	
+	public void setSpawner(int sl)
+	{
+		uint random = 0;
+		if((Spawner)GetNode("SpawnLoc" + sl) != null)
+		{
+			spawn = (Spawner)GetNode("SpawnLoc" + sl);
+			if(spawn.Position.y < -200)
+			{
+				random = GD.Randi() % 30 + 1;
+				spawn.setAngle((float) random);
+			} 
+			else
+			{
+				while((float) random < 30)
+				{
+					random = GD.Randi() % 60 + 1;
+					spawn.setAngle((float) random);
+				}
+				random = 0;
+			}
+			if(spawn.Position.x == 500)
+			{
+				spawn.setAngle(180 - spawn.getAngle());
+			}
+			spawn.setSpawnerForce(300);
 		}
 	}
 
 	public void spawnItem(int sl)
 	{
-		spawn = (Node2D)GetNode("SpawnLoc" + sl);
+		spawn = (Spawner)GetNode("SpawnLoc" + sl);
 		PackedScene missile = (PackedScene)GD.Load("res://Missile.tscn");
 		if(missile != null)
 		{
 			RigidBody2D missileFab = (RigidBody2D)missile.Instance();
 			missileFab.GlobalPosition = spawn.GlobalPosition;
 			AddChild(missileFab);
-			Node2D ship = (Node2D)GetNode("/root/Stage/BattleShip");
 			missileFab.Rotation = missileFab.Rotation + 90;
 		}
 	}
