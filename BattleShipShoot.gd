c3ba8b11d5e967dae3bb37948d668b1f08860a55
Addison <brauna2938@my.uwstout.edu>
2023-02-03 09:43:26 -0600

    Player Missile Timer

--- BattleShipShoot.gd
+++ BattleShipShoot.gd
@@ -4,8 +4,12 @@ signal spawn_missle(location)
 
 onready var muzzle = $Muzzle
 
+onready var fireDelayTimer = $FireDelayTimer
+var fireDelay: float = 0.5
+
 func _physics_process(delta):
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
+		fireDelayTimer.start(fireDelay)
 		shoot_missle()
 
 func shoot_missle():
