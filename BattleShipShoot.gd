d5adfedda856168c4626d33ef46aff12dba4e316
Addison <brauna2938@my.uwstout.edu>
2023-02-08 10:14:06 -0600

    Working Collision

--- BattleShipShoot.gd
+++ BattleShipShoot.gd
@@ -13,13 +13,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
 		fireDelayTimer.start(fireDelay)
 		shoot_missle()
+		#take_damage(1)
 
 func shoot_missle():
 	emit_signal("spawn_missle", muzzle.global_position)
 	
-func take_damage(damage):
-	hp -= damage
-	if hp <= 0:
-		queue_free()
+#func take_damage(damage):
+#	hp -= damage
+#	if hp <= 0:
+
 		
 
--- PlayerMissile.gd
+++ PlayerMissile.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var hp = 1
 
-var speed = 500
+var speed = 400
 
 func _ready():
 	set_physics_process(true)
@@ -14,9 +14,9 @@ func take_damage(damage):
 		queue_free()
 
 func _physics_process(delta):
-	var collideObject = move_and_collide(Vector2(0, -(speed * delta)))
-	if (collideObject):
-		take_damage(1)
+	global_position.y += -speed * delta
+	#var collideObject = move_and_collide(Vector2(0, -(speed * delta)))
+	#if (collideObject):
+	#	take_damage(1)
 
-#global_position.y += -speed * delta
 	
