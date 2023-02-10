bb88909cd934fc6a7a35f271f78f61d35d3a440e
Addison <brauna2938@my.uwstout.edu>
2023-02-10 16:22:09 -0600

    ShowPoints

--- EnemyMissileHealth.gd
+++ EnemyMissileHealth.gd
@@ -4,31 +4,42 @@ var hp = 1
 var explode = preload("res://smokeexplode.tscn")
 onready var world = get_node("/root/Stage")
 
-func _ready():
-	if self.get_name() == "Bomber1Area":
-		hp = 3
-	if self.get_name() == "Bomber2Area":
-		hp = 2
-
+var pointLow = preload("res://250Point.tscn")
+var pointMedium = preload("res://500Point.tscn")
+var pointHigh = preload("res://1000Point.tscn")
 
 func take_damage(damage):
-	print(hp)
 	hp -= damage
 	if hp <= 0:
-		
-		var blowup = explode.instance()
-		blowup.position = self.position
-		world.add_child(blowup)
-		#add_child(blowup)
-		blowup.global_position = get_global_position()
-		$CollisionShape2D.queue_free()
+ 		$CollisionShape2D.queue_free()
 		$"../SmokeParticles".queue_free()
 		$"../FireParticles".queue_free()
 		$"../Sprite".queue_free()
 		$"../CollisionShape2D".queue_free()
+				
+		var blowup = explode.instance()
+		var pointShow
+		
+
+		if ($"..".getPoints() == 250):
+			pointShow = pointLow.instance()
+		if ($"..".getPoints() == 500):
+			pointShow = pointMedium.instance()
+		if ($"..".getPoints() == 1000):
+			pointShow = pointHigh.instance()
+		
+		blowup.position = self.position
+		world.add_child(blowup)
+		blowup.global_position = get_global_position()
+		
+		pointShow.position = self.position
+		world.add_child(pointShow)
+		pointShow.global_position = get_global_position()
+		pointShow.global_position.y -= 50
 		
 		
 		yield(get_tree().create_timer(.9), "timeout")
 		blowup.queue_free()
+		pointShow.queue_free()
 		get_parent().queue_free()
 
--- EnemyPlaneHealth.gd
+++ EnemyPlaneHealth.gd
@@ -2,26 +2,42 @@ extends Area2D
 
 var hp = 1
 var explode = preload("res://smokeexplode.tscn")
+var pointLow = preload("res://2000Point.tscn")
+var pointMedium = preload("res://4000Point.tscn")
+var pointHigh = preload("res://8000Point.tscn")
+onready var world = get_node("/root/Stage")
+var pointShow
 
 func _ready():
-	if self.get_name() == "Bomber1Area":
-		hp = 3
-	if self.get_name() == "Bomber2Area":
-		hp = 2
+	if (self.get_name() == "Bomber1Area"):
+		#hp = 3
+		pointShow = pointLow.instance()
+	elif (self.get_name() == "Bomber2Area"):
+		#hp = 2
+		pointShow = pointMedium.instance()
+	else:
+		pointShow = pointHigh.instance()
 
 
 func take_damage(damage):
 	print(hp)
 	hp -= damage
 	if hp <= 0:
+		$CollisionShape2D.queue_free()
+		$"../BomberSprite".queue_free()
+		$"../CollisionPolygon2D".queue_free()
 		
 		var blowup = explode.instance()
 		add_child(blowup)
 		blowup.global_position = get_global_position()
-		$"../BomberSprite".queue_free()
-		$"../CollisionPolygon2D".queue_free()
 		
+		pointShow.position = self.position
+		world.add_child(pointShow)
+		pointShow.global_position = get_global_position()
+		pointShow.global_position.y -= 50
 		
 		yield(get_tree().create_timer(.9), "timeout")
+		blowup.queue_free()
+		pointShow.queue_free()
 		get_parent().queue_free()
 
