657cf698b825795b08a3d1e2fae67d121eaa234c
Addison <brauna2938@my.uwstout.edu>
2023-02-10 17:14:47 -0600

    CloudAlmostWork

--- World.gd
+++ World.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 var Missle = preload("res://PlayerInterceptor.tscn")
 
-#var Cloud = preload(CloudDay)
+var Cloud = preload("res://DayCloud.tscn")
+
+onready var cloudSpawnOne = $CloudSpawn1
+onready var cloudSpawnTwo = $CloudSpawn2
+
+onready var cloudSpawnTimer = $CloudSpawnTimer
+var cloudDelay: float = 0.5
 
 func _on_Battlecrip_spawn_missle(location):
 	var m = Missle.instance()
@@ -10,7 +16,22 @@ func _on_Battlecrip_spawn_missle(location):
 	add_child(m)
 
 #func _on_CloudDay_spawn_cloud(location):
-#	var c = Cloud.instance()
-#	c.global_position = location
-#	add_child(c)
+	
+func _ready():
+	var c = Cloud.instance()
+	c.global_position = cloudSpawnOne.global_position
+	add_child(c)
+	var d = Cloud.instance()
+	d.global_position = cloudSpawnTwo.global_position
+	add_child(d)
+	
+	#while(true):
+	#	if(cloudSpawnTimer.is_stopped()):
+	#		spawn_cloud()
+			
+func spawn_cloud():
+	var d = Cloud.instance()
+	d.global_position = cloudSpawnTwo.global_position
+	add_child(d)
+	
 
